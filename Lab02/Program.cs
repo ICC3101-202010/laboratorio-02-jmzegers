e86b90ba602fcce0a093b4658228afa4e6a7519e
Jose Miguel <jmzegers@miuandes.cl>
2020-03-27 09:59:14 -0300

    Second

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -2,11 +2,25 @@
 
 namespace Lab02
 {
-    class MainClass
+    class Cancion
     {
-        public static void Main(string[] args)
+        private string genero;
+        private string artista;
+        private string album;
+        private string nombre;
+
+        public string Informacion(string genCancion, string artCancion, string albumCancion, string nomCancion)
         {
-            Console.WriteLine("Hello World!");
+            this.genero = genCancion;
+            this.artista = artCancion;
+            this.album = albumCancion;
+            this.nombre = nomCancion;
+        }
+        static void Main(string[] args)
+        {
+            Cancion C1 = new Cancion();
+            C1.Informacion("Heavy Metal", "Sabaton", "Heroes", "No Bullets Fly");
+            Console.WriteLine(C1);
         }
     }
 }
