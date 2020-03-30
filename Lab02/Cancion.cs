088666499914f22123d7b603d450b6bf5811f81b
Jose Miguel <jmzegers@miuandes.cl>
2020-03-30 13:24:20 -0300

    Progreso

--- Lab02/Cancion.cs
+++ Lab02/Cancion.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lab02
 {
@@ -25,15 +29,11 @@ namespace Lab02
 
         public string[] Transformador(string genero, string artista, string album, string nombre)
         {
-            string[] a2;
-            a2[0] = genero;
-            a2[1] = artista;
-            a2[2] = album;
-            a2[3] = nombre;
+            string[] a2 = { genero, artista, album, nombre };
             return a2;
         }
 
-        public List<Array> listaCanciones = new List<Array>();
+        protected ArrayList listaCanciones = new ArrayList();
         public string[] t1;
         public string[] t2;
         public string[] t3;
@@ -72,42 +72,40 @@ namespace Lab02
         }
     }
 
+
     public class Espotifai : Cancion
     {
 
         public bool AgregarCancion(Cancion cancion)
         {
-            foreach (string[] t2 in listaCanciones)
+            foreach (string[] t in listaCanciones)
             {
-                if (t2[1] == t1[1] & t2[2] == t1[2] & t2[3] == t1[3])
+                if (t[1] == t1[1] & t[2] == t1[2] & t[3] == t1[3])
                 {
                     return false;
                 }
                 else
                 {
-                    listaCanciones.Add(t2);
+                    listaCanciones.Add(t);
                     return true;
                 }
             }
         }
+
         public void VerCanciones()
         {
             Console.WriteLine(listaCanciones);
         }
+
         public Cancion[] CancionesPorCriterio(String criterio, String valor)
         {
-            List<Array> listaResultados() = new List<Array>();
-            Console.WriteLine("Escriba por cual de los siguientes criterios desea hacer su busqueda de canciones");
-            Console.WriteLine("Genero");
-            Console.WriteLine("Artista");
-            Console.WriteLine("Album");
-            Console.WriteLine("Nombre");
+            ArrayList listaResultados = new ArrayList();
             criterio = Console.ReadLine();
             if (criterio == "Genero" || criterio == "genero")
             {
                 Console.WriteLine("Ingrese el genero deseado");
                 valor = Console.ReadLine();
-                foreach(String[] s in listaCanciones)
+                foreach (String[] s in listaCanciones)
                 {
                     if (valor == s[0])
                     {
@@ -118,14 +116,13 @@ namespace Lab02
                 {
                     Console.WriteLine("Error");
                     Console.WriteLine("No se han encontrado resultados para su busqueda");
-                    return listaResultados();
                 }
             }
             else if (criterio == "Artista" || criterio == "artista")
             {
                 Console.WriteLine("Ingrese el artista deseado");
                 valor = Console.ReadLine();
-                foreach(String[] s in listaCanciones)
+                foreach (String[] s in listaCanciones)
                 {
                     if (valor == s[1])
                     {
@@ -136,7 +133,6 @@ namespace Lab02
                 {
                     Console.WriteLine("Error");
                     Console.WriteLine("No se han encontrado resultados para su busqueda");
-                    return listaResultados();
                 }
             }
             else if (criterio == "Album" || criterio == "album")
@@ -154,7 +150,6 @@ namespace Lab02
                 {
                     Console.WriteLine("Error");
                     Console.WriteLine("No se han encontrado resultados para su busqueda");
-                    return listaResultados();
                 }
             }
             else if (criterio == "Nombre" || criterio == "nombre")
@@ -172,17 +167,53 @@ namespace Lab02
                 {
                     Console.WriteLine("Error");
                     Console.WriteLine("No se han encontrado resultados para su busqueda");
-                    return listaResultados();
                 }
             }
             else
             {
                 Console.WriteLine("Error");
                 Console.WriteLine("Criterio invalido");
-                return listaResultados();
+            }
+            return listaResultados();
+        }
+
+        ArrayList playlist = new ArrayList();
+        ArrayList opciones = new ArrayList();
+        private string respuesta;
+        private string resp;
+        private int r;
+        public bool GenerarPlaylist(string criterio, string valorCriterio, string nombrePlaylist)
+        {
+            Console.WriteLine("Desea agregar canciones a su playlist? si/no");
+            respuesta = Console.ReadLine();
+            if (respuesta == "si")
+            {
+                while (respuesta == "si")
+                {
+                    Console.WriteLine("Escriba por cual de los siguientes criterios desea hacer su busqueda de canciones");
+                    Console.WriteLine("Genero");
+                    Console.WriteLine("Artista");
+                    Console.WriteLine("Album");
+                    Console.WriteLine("Nombre");
+                    resp = Console.ReadLine();
+                    opciones = CancionesPorCriterio(respuesta, criterio);
+                    Console.WriteLine("Escriba el numero de la cancion que desea agregar");
+                    int r = Convert.ToInt32(Console.ReadLine());
+                    playlist.Add(opciones(r + 1));
+                    Console.WriteLine("Desea agregar otra cancion a su playlist? si/no");
+                    respuesta = Console.ReadLine();
+                }
             }
         }
         public void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Playlist : Cancion
+    {
+        public Playlist(Cancion cancion)
         {
 
         }
